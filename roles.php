f870f88b22411b7154bed2d912cc937a4c57aed5
oscarbolivar <oibc84@hotmail.com>
2019-06-07 18:07:44 -0500

    Añadimos Bootstrap

--- roles.php
+++ roles.php
@@ -19,7 +19,7 @@ Gestor de Permisos a los Modulos.
 /* Funcion verificar usuarios */
 	function revisar()
 	{
-		alert(document.f.us.value);
+		// alert(document.f.us.value);
 		
 		if(document.f.us.value!='')
 		{	
@@ -54,113 +54,123 @@ Gestor de Permisos a los Modulos.
 	<head>
 		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 		<TITLE>Permisos de Usuario</TITLE>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<link rel="stylesheet" href="js/bootstrap/css/bootstrap.min.css" />
 		<link rel="StyleSheet" href="css/estilos.css" type="text/css"> 
 	</head>
 	<body>
 	<!-- Titulo animado -->
-	<marquee><font size=7 color="black">Conceder Permisos por Usuario</font></marquee>
-	<BR><BR>
-	<!-- Logo animado -->
-		<div class="container">
-			<div class="div-img">
-				<img class="img" src="img/logo.gif" title="logo" alt="logo">
+		<!-- <marquee><font size=7 color="black">Conceder Permisos por Usuario</font></marquee> -->
+		<!-- Logo animado -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col">
+					<div class="card">
+						<h5 class="card-header">Conceder Permisos por Usuario</h5>
+						<div class="card-body">
+							<!-- <div class="div-img">
+								<img class="img" src="img/logo.gif" title="logo" alt="logo">
+							</div> -->
+							<!-- Formulario Agregar / Quitar aplicaciones usuarios -->			
+							<form name="f">
+								<div class="row">
+									<div class="col-lg-4 col-md-8 col-sm-12">
+										<font color="black">Usuario</font>&nbsp;<br><br>
+										<select name="us" id="us" class="form-control">
+											<option value=''>-Seleccione-</option>
+											<?php
+												$sql = "SELECT * FROM usuarios";
+												$result = $conn->query($sql);
+												$total=$result->num_rows;
+												if($total>0)
+													{
+													for($i=1;$i<=$total;$i++)
+														{
+															$row = $result->fetch_assoc();
+															$id_c=$row["id"];
+															$nombre_c=$row["nombre"];
+															$apelldos_c=$row["apellidos"];
+															$nom=$nombre_c.' '.$apelldos_c;
+															
+														if($us==$id_c)
+															{
+																echo "
+																<option value='$id_c' selected>$nom</option>
+																";
+															}	
+														else
+															{
+																echo "
+																<option value='$id_c' >$nom</option>
+																";
+															}		
+													}
+												}	
+											?>
+										</select>
+									</div>
+								</div>
+								<div class="row">&nbsp;</div>
+								<div class="row">
+									<div class="col-4 d-flex justify-content-end">
+										<!-- Boton enviar formulario -->
+										<button type="button" name="b" class="btn btn-outline-secondary" onclick="revisar();">Continuar</button>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-8"><?php
+										if ($us != '') {?>
+											<!-- Titulo tabla de registros usuarios -->
+											<br><br>
+											<h4>Lista de Aplicaciones</h4>
+											<h6>Usuario = <?=$us?></h6>
+											<table class='table table-hover'>
+												<thead>
+													<tr>
+														<th scope="col" class="text-center">Aplicación</th>
+														<th scope="col" class="text-center">Habilitar</th>
+													</tr>
+												</thead>
+												<tbody><?php
+													/* Consulta registros aplicaciones */
+													$sql = "SELECT * FROM aplicaciones";
+													$result = $conn->query($sql);
+													$total=$result->num_rows;
+													if ($total > 0) {
+														for ($j=1;$j<=$total;$j++) {
+															$row = $result->fetch_assoc();
+															$id_c1=$row["id"];
+															$nombre_c1=$row["nombre"];
+															$url_c1=$row["url"];
+															$nom=$nombre_c.' '.$apelldos_c;?>
+															<tr>
+																<td class="text-center"><?=$nombre_c1?></td><?php
+																/* Consulta registros permisos de aplicaciones por usuario */
+																$sql1 = "SELECT * FROM usu_apli where id_usuario='$us' and id_apli='$id_c1'";
+																$result1 = $conn->query($sql1);
+																$total1=$result1->num_rows;
+																if ($total1>0) {?>
+																	<td class="text-center"><input type='checkbox' name='<?=$id_c1?>' onclick='accion("E","<?=$us?>","<?=$id_c1?>");' checked></td><?php
+																} else {?>
+																	<td class="text-center"><input type='checkbox' name='<?=$id_c1?>'  onclick='accion("I","<?=$us?>","<?=$id_c1?>");'></td><?php
+																}?>
+															</tr><?php
+														}
+													}?>
+												</tbody>
+											<table><?php
+										}?>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-		<BR><BR><BR><BR>
-		<BR><BR><BR><BR>
-		<BR><BR><BR><BR>
-		<center>
-<!-- Formulario Agregar / Quitar aplicaciones usuarios -->			
-		<form name=f>
-			<font color="black">Usuario</font>&nbsp;<br><br>
-			<select name=us id=us>
-				<option value=''>-Seleccione-</option>
-				<?php
-					$sql = "SELECT * FROM usuarios";
-					$result = $conn->query($sql);
-					$total=$result->num_rows;
-					if($total>0)
-						{
-						for($i=1;$i<=$total;$i++)
-							{
-								$row = $result->fetch_assoc();
-								$id_c=$row["id"];
-								$nombre_c=$row["nombre"];
-								$apelldos_c=$row["apellidos"];
-								$nom=$nombre_c.' '.$apelldos_c;
-								
-							if($us==$id_c)
-								{
-									echo "
-									<option value='$id_c' selected>$nom</option>
-									";
-								}	
-							else
-								{
-									echo "
-									<option value='$id_c' >$nom</option>
-									";
-								}		
-						}
-					}	
-				?>
-			</select>
-			<br><br>
-	<!-- Boton enviar formulario -->			
-		<input type=button name=b value='Continuar' onclick='revisar();'><br><br>
-		<?php
-			if($us!='')
-			{
-				echo "
-			<!-- Titulo tabla de registros usuarios -->
-				<br><br>
-				<font size=6 color=black>Lista de Aplicaciones
-				</font><br><br>";
-				echo "
-				<font color=black>Usuario = ".$us."</font><br><br>";
-				echo "
-				<table border=1>
-					<tr>
-						<td align=center bgcolor=white>Aplicación</td>
-						<td align=center bgcolor=white>Habilitar</td>";
-				/* Consulta registros aplicaciones */
-					$sql = "SELECT * FROM aplicaciones";
-					$result = $conn->query($sql);
-					$total=$result->num_rows;
-					if($total>0)
-					{
-						for($j=1;$j<=$total;$j++)
-						{
-							$row = $result->fetch_assoc();
-							$id_c1=$row["id"];
-							$nombre_c1=$row["nombre"];
-							$url_c1=$row["url"];
-							$nom=$nombre_c.' '.$apelldos_c;
-							
-					echo "
-					</tr>
-					<tr>
-						<td align=center bgcolor=white>$nombre_c1</td>";
-						/* Consulta registros permisos de aplicaciones por usuario */
-							$sql1 = "SELECT * FROM usu_apli where id_usuario='$us' and id_apli='$id_c1'";
-							$result1 = $conn->query($sql1);
-							$total1=$result1->num_rows;
-							if($total1>0)
-							{
-								echo "<td><input type='checkbox' name='$id_c1' onclick='accion(\"E\",\"$us\",\"$id_c1\");' checked></td>";							
-							}	
-							else
-							{
-								echo "<td><input type='checkbox' name='$id_c1'  onclick='accion(\"I\",\"$us\",\"$id_c1\");'></td>";							
-							}
-					echo "
-					</tr>";
-						}
-					}	
-					echo "
-				</table>";
-			}	
-		?>
-		</form>
+
+		<script src="js/jquery/jquery-3.3.1.slim.min.js"></script>
+		<script src="js/bootstrap/js/popper.min.js"></script>
+		<script src="js/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>	
